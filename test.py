26699adda8678a18e5ffe2fbb83ff08a320904ed
kozlov37 <39125346+kozlov37@users.noreply.github.com>
2018-05-12 18:19:54 +0300

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-c1 = {"city": "Moscow", "temperature": 25, "wind": True}
-c2 = {"city": "Kaliningrad", "temperature": 20, "wind": False}
-c3 = {"city": "Kiyv", "temperature": 22, "wind": False}
-db1 = {"Anton": c1, "Yuriy": c2, "Alexey": c3}
-naming = input('Как вас зовут?')
-print(db1.get(naming))
-naming2 = input('Как вас зовут?')
